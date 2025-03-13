c042bcfeb7c36142aa7ab7c1aee6ddda8ed3b456
Misho1019 <mikailtorres99@gmail.com>
2025-03-13 06:23:10 +0200

    Get all games

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router";
+
+import gameService from "../../services/gameService"
+
 export default function GameCreate() {
+    const navigate = useNavigate();
+
+    const submitAction = async (formData) => {
+        const gameData = Object.fromEntries(formData)
+
+        await gameService.create(gameData)
+
+        navigate('/games')
+    }
     return (
         // <!-- Create Page ( Only for logged-in users ) -->
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" action={submitAction}>
                 <div className="container">
 
                     <h1>Create Game</h1>
