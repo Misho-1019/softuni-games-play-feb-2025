d7e1db877c56921348eef3653445a74f51ada29b
Misho1019 <mikailtorres99@gmail.com>
2025-03-22 03:47:16 +0200

    Add use logout with custom headers

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,5 +1,6 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import request from "../utils/request";
+import { UserContext } from "../context/UserContext";
 
 const baseUrl = 'http://localhost:3030/users';
 
@@ -33,5 +34,21 @@ export const useRegister = () => {
 
     return {
         register,
+    }
+}
+
+export const useLogout = () => {
+    const { accessToken } = useContext(UserContext);
+
+    const options = {
+        headers: {
+            'X-Authorization': accessToken,
+        }
+    }
+
+    const logout = () => request.get(`${baseUrl}/logout`, null, options)
+
+    return {
+        logout,
     }
 }
