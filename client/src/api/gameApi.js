450dcc54e2ea688fc6bb8ab309b25537e651f66b
Misho1019 <mikailtorres99@gmail.com>
2025-03-23 03:11:48 +0200

    Add latest games

--- client/src/api/gameApi.js
+++ client/src/api/gameApi.js
@@ -30,6 +30,24 @@ export const useGame = (gameId) => {
     }
 }
 
+export const useLatestGames = () => {
+    const [latestGames, setLatestGames] = useState([])
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams({
+            sortBy: '_createdOn desc',
+            pageSize: 3,
+        })
+
+        request.get(`${baseUrl}?${searchParams.toString()}`)
+            .then(setLatestGames)
+    }, [])
+    
+    return {
+        latestGames,
+    }
+}
+
 export const useCreateGame = () => {
     const { request } = useAuth()
 
